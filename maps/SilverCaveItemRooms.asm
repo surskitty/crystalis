7792db947799120abda70121576c49716c022db2
surskitty <surskitty@gmail.com>
2017-09-28 19:25:53 -0400

    And Mewtwo!  That makes this complete. #4

--- maps/SilverCaveItemRooms.asm
+++ maps/SilverCaveItemRooms.asm
@@ -1,13 +1,49 @@
 const_value set 2
 	const SILVERCAVEITEMROOMS_POKE_BALL1
 	const SILVERCAVEITEMROOMS_POKE_BALL2
+	const SILVERCAVEITEMROOMS_MEWTWO
 
 SilverCaveItemRooms_MapScriptHeader:
 .MapTriggers:
 	db 0
 
 .MapCallbacks:
-	db 0
+	db 1
+	dbw MAPCALLBACK_OBJECTS, .Mewtwo
+
+.Mewtwo:
+	checkevent EVENT_FOUGHT_MEWTWO
+	iftrue .NoAppear
+	checkevent EVENT_RESTORED_POWER_TO_KANTO
+	iftrue .Appear
+	jump .NoAppear
+
+.Appear:
+	appear SILVERCAVEITEMROOMS_MEWTWO
+	return
+
+.NoAppear:
+	disappear SILVERCAVEITEMROOMS_MEWTWO
+	return
+
+Mewtwo:
+	faceplayer
+	opentext
+	writetext MewtwoText
+	cry MEWTWO
+	pause 15
+	closetext
+	setevent EVENT_FOUGHT_MEWTWO
+	writecode VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon MEWTWO, 70
+	startbattle
+	disappear SILVERCAVEITEMROOMS_MEWTWO
+	reloadmapafterbattle
+	end
+
+MewtwoText:
+	text "Mew!"
+	done
 
 SilverCaveItemRoomsMaxRevive:
 	itemball MAX_REVIVE
@@ -31,6 +67,7 @@ SilverCaveItemRooms_MapEventHeader:
 	db 0
 
 .PersonEvents:
-	db 2
+	db 3
 	person_event SPRITE_POKE_BALL, 3, 6, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, SilverCaveItemRoomsMaxRevive, EVENT_SILVER_CAVE_ITEM_ROOMS_MAX_REVIVE
 	person_event SPRITE_POKE_BALL, 11, 15, SPRITEMOVEDATA_ITEM_TREE, 0, 0, -1, -1, 0, PERSONTYPE_ITEMBALL, 0, SilverCaveItemRoomsFullRestore, EVENT_SILVER_CAVE_ITEM_ROOMS_FULL_RESTORE
+	person_event SPRITE_RHYDON, 7, 15, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, (1 << 3) | PAL_OW_BLUE, PERSONTYPE_SCRIPT, 0, Mewtwo, EVENT_MT_SILVER_MEWTWO
