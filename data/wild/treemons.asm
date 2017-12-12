b03d8373d111661160fafa00bd9b444519bf0370
surskitty <surskitty@gmail.com>
2017-12-12 01:11:21 -0500

    Reapplying certain changes.

--- data/wild/treemons.asm
+++ data/wild/treemons.asm
@@ -14,16 +14,15 @@ TreeMons: ; b82e8
 ;	db  %, species, level
 
 TreeMons1: ; b82fa
-; common
 	db 50, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 15, SPEAROW,    10
-	db 10, AIPOM,      10
+	db 15, MANKEY,     10
+	db 15, AIPOM,      10
+	db 10, MANKEY,     10
 	db  5, AIPOM,      10
 	db  5, AIPOM,      10
 	db -1
-; rare
-	db 50, SPEAROW,    10
+
+	db 50, MURKROW,    10
 	db 15, HERACROSS,  10
 	db 15, HERACROSS,  10
 	db 10, AIPOM,      10
@@ -32,16 +31,15 @@ TreeMons1: ; b82fa
 	db -1
 
 TreeMons2: ; b8320
-; common
 	db 50, SPEAROW,    10
 	db 15, EKANS,      10
-	db 15, SPEAROW,    10
+	db 15, MURKROW,    10
 	db 10, AIPOM,      10
 	db  5, AIPOM,      10
 	db  5, AIPOM,      10
 	db -1
-; rare
-	db 50, SPEAROW,    10
+
+	db 50, GLIGAR,     10
 	db 15, HERACROSS,  10
 	db 15, HERACROSS,  10
 	db 10, AIPOM,      10
@@ -50,25 +48,23 @@ TreeMons2: ; b8320
 	db -1
 
 TreeMons3: ; b8346
-; common
-	db 50, HOOTHOOT,   10
+	db 50, EXEGGCUTE,  10
 	db 15, SPINARAK,   10
 	db 15, LEDYBA,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 10, HOOTHOOT,   10
+	db  5, PARAS,      10
+	db  5, NOCTOWL,    10
 	db -1
-; rare
-	db 50, HOOTHOOT,   10
+
+	db 50, EXEGGCUTE,  10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
-	db 10, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
-	db  5, EXEGGCUTE,  10
+	db 10, PINSIR,     10
+	db  5, SCYTHER,    10
+	db  5, PINSIR,     10
 	db -1
 
 TreeMons4: ; b836c
-; common
 	db 50, HOOTHOOT,   10
 	db 15, EKANS,      10
 	db 15, HOOTHOOT,   10
@@ -76,8 +72,8 @@ TreeMons4: ; b836c
 	db  5, EXEGGCUTE,  10
 	db  5, EXEGGCUTE,  10
 	db -1
-; rare
-	db 50, HOOTHOOT,   10
+
+	db 50, FARFETCH_D, 10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
 	db 10, EXEGGCUTE,  10
@@ -86,16 +82,15 @@ TreeMons4: ; b836c
 	db -1
 
 TreeMons5: ; b8392
-; common
 	db 50, HOOTHOOT,   10
 	db 15, VENONAT,    10
-	db 15, HOOTHOOT,   10
+	db 15, NATU,       10
 	db 10, EXEGGCUTE,  10
 	db  5, EXEGGCUTE,  10
 	db  5, EXEGGCUTE,  10
 	db -1
-; rare
-	db 50, HOOTHOOT,   10
+
+	db 50, NATU,       10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
 	db 10, EXEGGCUTE,  10
@@ -104,7 +99,6 @@ TreeMons5: ; b8392
 	db -1
 
 TreeMons6: ; b83b8
-; common
 	db 50, HOOTHOOT,   10
 	db 15, PINECO,     10
 	db 15, PINECO,     10
@@ -112,7 +106,7 @@ TreeMons6: ; b83b8
 	db  5, BUTTERFREE, 10
 	db  5, BEEDRILL,   10
 	db -1
-; rare
+
 	db 50, HOOTHOOT,   10
 	db 15, CATERPIE,   10
 	db 15, WEEDLE,     10
