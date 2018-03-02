c799b6b72078b856423011125577b319f384e2e8
surskitty <surskitty@gmail.com>
2018-03-02 15:57:54 -0500

    Making 1/8 female gender ratios 1/4 female.

--- data/pokemon/base_stats/togetic.asm
+++ data/pokemon/base_stats/togetic.asm
@@ -7,7 +7,7 @@
 	db 75 ; catch rate
 	db 114 ; base exp
 	db NO_ITEM, NO_ITEM ; items
-	db GENDER_F12_5 ; gender ratio
+	db GENDER_F25 ; gender ratio
 	db 100 ; unknown 1
 	db 10 ; step cycles to hatch
 	db 5 ; unknown 2
